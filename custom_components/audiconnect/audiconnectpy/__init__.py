38ae670127a828d1c7b8119c01b2e37208b8f4c9
cyr-ius <cyr-ius@ipocus.net>
2023-04-23 14:18:01 +0200

    Remove api (transfer to pypi)

--- custom_components/audiconnect/audiconnectpy/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding:utf-8 -*-
-
-"""audiconnectpy package."""
-from .api import AudiConnect
-from .exceptions import AudiException, AuthorizationError
-
-__all__ = ["AudiConnect", "AudiException", "AuthorizationError"]
