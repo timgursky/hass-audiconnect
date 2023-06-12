f473a4068a2435f0d7de77836c421a53be35ce96
cyr-ius <cyr-ius@ipocus.net>
2023-06-12 18:42:05 +0200

    Fix already configured

--- custom_components/audiconnect/config_flow.py
+++ custom_components/audiconnect/config_flow.py
@@ -8,7 +8,7 @@ import voluptuous as vol
 
 from homeassistant import config_entries
 from homeassistant.const import CONF_PASSWORD, CONF_PIN, CONF_USERNAME
-from homeassistant.data_entry_flow import FlowResult
+from homeassistant.data_entry_flow import FlowResult, AbortFlow
 from homeassistant.helpers.aiohttp_client import async_create_clientsession
 
 from audiconnectpy import AudiConnect, AudiException, AuthorizationError
@@ -16,7 +16,7 @@ from .const import CONF_COUNTRY, COUNTRY_CODE, DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
-STEP_USER_DATA_SCHEMA = vol.Schema(
+DATA_SCHEMA = vol.Schema(
     {
         vol.Required(CONF_USERNAME): str,
         vol.Required(CONF_PASSWORD): str,
@@ -30,16 +30,19 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Audi connect."""
 
     VERSION = 1
+    CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_POLL
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
     ) -> FlowResult:
         """Handle the initial step."""
         errors = {}
-        if user_input:
+        if user_input is not None:
             try:
                 self._async_abort_entries_match(
-                    {CONF_USERNAME: user_input[CONF_USERNAME]}
+                    {
+                        CONF_USERNAME: user_input[CONF_USERNAME],
+                    },
                 )
                 connection = AudiConnect(
                     async_create_clientsession(self.hass),
@@ -56,12 +59,9 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 errors["base"] = "invalid_auth"
             except AudiException:
                 errors["base"] = "cannot_connect"
-            except Exception:  # pylint: disable=broad-except
-                _LOGGER.exception("Unexpected exception")
-                errors["base"] = "unknown"
             else:
                 return self.async_create_entry(title="Audi connect", data=user_input)
 
         return self.async_show_form(
-            step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
+            step_id="user", data_schema=DATA_SCHEMA, errors=errors
         )
