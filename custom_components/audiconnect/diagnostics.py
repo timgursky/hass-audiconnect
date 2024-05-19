6c30f3e456d248656b60c06504f07be5bfbf957d
cyr-ius <cyr-ius@ipocus.net>
2024-05-19 19:55:29 +0200

    Update diagnostic

--- custom_components/audiconnect/diagnostics.py
+++ custom_components/audiconnect/diagnostics.py
@@ -1,4 +1,5 @@
 """Diagnostics support for Audi Connect."""
+
 from __future__ import annotations
 
 from collections.abc import Callable
@@ -46,6 +47,7 @@ TO_REDACT = {
     "placeOfBirth",
     "carnetEnrollmentCountry",
     "spin",
+    "customTitle",
 }
 
 
@@ -54,54 +56,38 @@ async def async_get_config_entry_diagnostics(
 ) -> dict[str, Any]:
     """Return diagnostics for a config entry."""
     coordinator = hass.data[DOMAIN][entry.entry_id]
-    _datas = {}
 
     async def diag(func: Callable[..., Any], *args: Any) -> None:
-        rslt = {}
         with suppress(Exception):
             rsp = await func(*args)
-            rslt = (
+            return (
                 rsp
                 if isinstance(rsp, dict | list | set | float | int | str | tuple)
                 else vars(rsp)
             )
 
-        _datas[i].update({func.__name__.replace("async_get_", ""): rslt})
+    information_vehicles = {
+        "async_get_information_vehicles": await diag(
+            coordinator.api.async_get_information_vehicles
+        )
+    }
 
-    i = 0
-    for vehicle in coordinator.data.values():
-        i += 1
-        _datas.update({i: vars(vehicle)})
-        await diag(vehicle.async_get_vehicle_details)
-        await diag(vehicle.async_get_vehicle)
-        await diag(vehicle.async_get_stored_position)
-        await diag(vehicle.async_get_destinations)
-        await diag(vehicle.async_get_history)
-        await diag(vehicle.async_get_vehicule_users)
-        await diag(vehicle.async_get_charger)
-        await diag(vehicle.async_get_tripdata, "cyclic")
-        await diag(vehicle.async_get_tripdata, "longTerm")
-        await diag(vehicle.async_get_tripdata, "shortTerm")
-        await diag(vehicle.async_get_operations_list)
-        await diag(vehicle.async_get_climater)
-        await diag(vehicle.async_get_preheater)
-        await diag(vehicle.async_get_climater_timer)
-        await diag(vehicle.async_get_capabilities)
-        await diag(vehicle.async_get_honkflash)
-        # await diag(vehicle.async_get_personal_data)
-        await diag(vehicle.async_get_real_car_data)
-        await diag(vehicle.async_get_mbb_status)
-        await diag(vehicle.async_get_identity_data)
-        await diag(vehicle.async_get_users)
-        await diag(vehicle.async_get_fences)
-        await diag(vehicle.async_get_fences_config)
-        await diag(vehicle.async_get_speed_alert)
-        await diag(vehicle.async_get_speed_config)
+    vehicles = {}
+    for idx, vehicle in enumerate(coordinator.data):
+        functions = {
+            "async_get_capabilities": await diag(vehicle.async_get_capabilities),
+            "async_get_selectivestatus": await diag(vehicle.async_get_selectivestatus),
+        }
+        vehicle_dict = vehicle.to_dict()
+        vehicle_dict.update(**functions)
+        vehicle_dict.pop("location", None)
+        vehicles.update({idx: vehicle_dict})
 
     return {
         "entry": {
             "data": async_redact_data(entry.data, TO_REDACT),
             "options": async_redact_data(entry.options, TO_REDACT),
         },
-        "data": async_redact_data(_datas, TO_REDACT),
+        "information_vehicles": async_redact_data(information_vehicles, TO_REDACT),
+        "vehicles": async_redact_data(vehicles, TO_REDACT),
     }
